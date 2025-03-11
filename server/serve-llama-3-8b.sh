d3b70959656c6b5286673d1d5796a7dcf29fd5cf
Ubuntu <azureuser@fiodar-a100-vmss-1-vmss000001.fall3ublhamenklcu35hj2caof.jx.internal.cloudapp.net>
2025-03-11 23:26:40 +0000

    llama-3-8b results

--- server/serve-llama-3-8b.sh
+++ server/serve-llama-3-8b.sh
@@ -1,4 +1,2 @@
 cd ~/powertrace-sim/server/
-vllm serve meta-llama/Llama-3.1-8B-Instruct
-\ --tensor-parallel-size ${TENSOR_PARALLEL_SIZE}
-\ --api-key ${OPENAI_API_KEY}
+vllm serve meta-llama/Llama-3.1-8B-Instruct --tensor-parallel-size ${TENSOR_PARALLEL_SIZE} --api-key ${OPENAI_API_KEY}
