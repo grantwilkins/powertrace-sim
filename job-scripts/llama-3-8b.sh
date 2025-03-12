a228067d315d584de63b6ef5cf6a88aa104a831a
Grant <grant.wilkins371@gmail.com>
2025-03-12 11:55:21 -0700

    change cuda visibility and polling frequency

--- job-scripts/llama-3-8b.sh
+++ job-scripts/llama-3-8b.sh
@@ -1,12 +1,12 @@
 TENSOR_PARALLEL_SIZES=(1 2 4 8)
 for TENSOR_PARALLEL_SIZE in ${TENSOR_PARALLEL_SIZES[@]}; do
-    if [${TENSOR_PARALLEL_SIZE} -eq 1]; then
+    if [ ${TENSOR_PARALLEL_SIZE} -eq 1 ]; then
         export CUDA_VISIBLE_DEVICES=0
-    elif [${TENSOR_PARALLEL_SIZE} -eq 2]; then
+    elif [ ${TENSOR_PARALLEL_SIZE} -eq 2 ]; then
         export CUDA_VISIBLE_DEVICES=0,1
-    elif [${TENSOR_PARALLEL_SIZE} -eq 4]; then
+    elif [ ${TENSOR_PARALLEL_SIZE} -eq 4 ]; then
         export CUDA_VISIBLE_DEVICES=0,1,2,3
-    elif [${TENSOR_PARALLEL_SIZE} -eq 8]; then
+    elif [ ${TENSOR_PARALLEL_SIZE} -eq 8 ]; then
         export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
     fi
     export TENSOR_PARALLEL_SIZE=${TENSOR_PARALLEL_SIZE}
@@ -21,7 +21,7 @@ for TENSOR_PARALLEL_SIZE in ${TENSOR_PARALLEL_SIZES[@]}; do
     for POISSON_ARRIVAL_RATE in ${POISSON_ARRIVAL_RATES[@]}; do
         DATE_TIME=$(date '+%Y-%m-%d-%H-%M-%S')
         touch llama-3-8b_tp${TENSOR_PARALLEL_SIZE}_p${POISSON_ARRIVAL_RATE}_d${DATE_TIME}.csv
-        nvidia-smi --query-gpu=timestamp,power.draw,utilization.gpu,memory.used --format=csv -l 0.1 >> llama-3-8b_tp${TENSOR_PARALLEL_SIZE}_p${POISSON_ARRIVAL_RATE}_d${DATE_TIME}.csv &
+        nvidia-smi --query-gpu=timestamp,power.draw,utilization.gpu,memory.used --format=csv -lms 250 >> llama-3-8b_tp${TENSOR_PARALLEL_SIZE}_p${POISSON_ARRIVAL_RATE}_d${DATE_TIME}.csv &
         NVIDIA_SMI_PID=$!
         python3 client.py --model-name meta-llama/Llama-3.1-8B-Instruct --api-key ${OPENAI_API_KEY} --tensor-parallel-size ${TENSOR_PARALLEL_SIZE} --poisson-arrival-rate ${POISSON_ARRIVAL_RATE}
         kill -9 ${NVIDIA_SMI_PID}
