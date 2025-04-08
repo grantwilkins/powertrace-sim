a740d0933babfd0533b259e4e27fe98b3b37933e
Ubuntu <azureuser@fiodar-a100-vmss-1-vmss000001.fall3ublhamenklcu35hj2caof.jx.internal.cloudapp.net>
2025-04-08 17:40:41 +0000

    some more Llama-3-8b power traces

--- job-scripts/llama-3-8b.sh
+++ job-scripts/llama-3-8b.sh
@@ -1,4 +1,4 @@
-TENSOR_PARALLEL_SIZES=(2)
+TENSOR_PARALLEL_SIZES=(2 4 8)
 for TENSOR_PARALLEL_SIZE in ${TENSOR_PARALLEL_SIZES[@]}; do
     if [ ${TENSOR_PARALLEL_SIZE} -eq 1 ]; then
         export CUDA_VISIBLE_DEVICES=0
@@ -12,12 +12,13 @@ for TENSOR_PARALLEL_SIZE in ${TENSOR_PARALLEL_SIZES[@]}; do
     export TENSOR_PARALLEL_SIZE=${TENSOR_PARALLEL_SIZE}
     cd ~/powertrace-sim/server
     bash serve-llama-3-8b.sh &
+    export SERVING_PID=$!
     while ! curl -s http://localhost:8000/v1/completions > /dev/null; do
         echo "Waiting for server to start..."
         sleep 10
     done
     cd ~/powertrace-sim/client
-    POISSON_ARRIVAL_RATES=(1.0 2.0 4.0)
+    POISSON_ARRIVAL_RATES=(0.125 0.25 0.5 1.0 2.0 4.0)
     for POISSON_ARRIVAL_RATE in ${POISSON_ARRIVAL_RATES[@]}; do
         DATE_TIME=$(date '+%Y-%m-%d-%H-%M-%S')
         touch llama-3-8b_tp${TENSOR_PARALLEL_SIZE}_p${POISSON_ARRIVAL_RATE}_d${DATE_TIME}.csv
@@ -26,5 +27,5 @@ for TENSOR_PARALLEL_SIZE in ${TENSOR_PARALLEL_SIZES[@]}; do
         python3 client.py --model-name meta-llama/Llama-3.1-8B-Instruct --api-key ${OPENAI_API_KEY} --tensor-parallel-size ${TENSOR_PARALLEL_SIZE} --poisson-arrival-rate ${POISSON_ARRIVAL_RATE}
         kill -9 ${NVIDIA_SMI_PID}
     done
-    pkill -9 -f "vllm serve"
+    pkill -9 ${SERVING_PID}
 done
