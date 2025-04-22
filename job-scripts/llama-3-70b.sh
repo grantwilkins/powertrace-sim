9048b100029c368b89d576b15816845a5af959c0
Ubuntu <azureuser@fiodar-a100-vmss-1-vmss000001.fall3ublhamenklcu35hj2caof.jx.internal.cloudapp.net>
2025-04-22 18:42:17 +0000

    update scripts

--- job-scripts/llama-3-70b.sh
+++ job-scripts/llama-3-70b.sh
@@ -13,7 +13,7 @@ for TENSOR_PARALLEL_SIZE in ${TENSOR_PARALLEL_SIZES[@]}; do
     cd ~/powertrace-sim/server
     bash serve-llama-3-70b.sh &
     export SERVING_PID=$!
-    while ! curl -s http://localhost:8000/v1/completions > /dev/null; do
+    while ! curl -s -f http://localhost:8000/health &> /dev/null; do
         echo "Waiting for server to start..."
         sleep 10
     done
@@ -24,7 +24,7 @@ for TENSOR_PARALLEL_SIZE in ${TENSOR_PARALLEL_SIZES[@]}; do
         touch llama-3-70b_tp${TENSOR_PARALLEL_SIZE}_p${POISSON_ARRIVAL_RATE}_d${DATE_TIME}.csv
         nvidia-smi --query-gpu=timestamp,power.draw,utilization.gpu,memory.used --format=csv -lms 250 >> llama-3-70b_tp${TENSOR_PARALLEL_SIZE}_p${POISSON_ARRIVAL_RATE}_d${DATE_TIME}.csv &
         NVIDIA_SMI_PID=$!
-        python3 client.py --model-name meta-llama/Llama-3.1-8B-Instruct --api-key ${OPENAI_API_KEY} --tensor-parallel-size ${TENSOR_PARALLEL_SIZE} --poisson-arrival-rate ${POISSON_ARRIVAL_RATE} --date ${DATE_TIME}
+        python3 client.py --model-name meta-llama/Llama-3.1-70B-Instruct --api-key ${OPENAI_API_KEY} --tensor-parallel-size ${TENSOR_PARALLEL_SIZE} --poisson-arrival-rate ${POISSON_ARRIVAL_RATE} --date ${DATE_TIME}
         kill -9 ${NVIDIA_SMI_PID}
     done
     pkill -9 ${SERVING_PID}
