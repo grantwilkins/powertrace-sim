9cb467e3ff5d0c223e6c1256f040c2c394744827
Grant <grant.wilkins371@gmail.com>
2025-04-25 17:18:09 -0700

    poisson rates

--- job-scripts/llama-3-8b.sh
+++ job-scripts/llama-3-8b.sh
@@ -10,11 +10,7 @@ for TENSOR_PARALLEL_SIZE in ${TENSOR_PARALLEL_SIZES[@]}; do
         sleep 10
     done
     cd ~/powertrace-sim/client
-    POISSON_ARRIVAL_RATES=(0.125 0.25 0.5 1.0 2.0 4.0)
-    NUM_ITERATIONS=5
-    for ((i=0; i<NUM_ITERATIONS; i++)); do
-        POISSON_ARRIVAL_RATES+=(${POISSON_ARRIVAL_RATES[@]})
-    done
+    POISSON_ARRIVAL_RATES=(0.125 0.25 0.5 1.0 2.0 4.0 0.125 0.25 0.5 1.0 2.0 4.0 0.125 0.25 0.5 1.0 2.0 4.0 0.125 0.25 0.5 1.0 2.0 4.0 0.125 0.25 0.5 1.0 2.0 4.0)
     for POISSON_ARRIVAL_RATE in ${POISSON_ARRIVAL_RATES[@]}; do
         DATE_TIME=$(date '+%Y-%m-%d-%H-%M-%S')
         touch llama-3-8b_tp${TENSOR_PARALLEL_SIZE}_p${POISSON_ARRIVAL_RATE}_d${DATE_TIME}.csv
