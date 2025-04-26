a7bc5ec1d953902833f8ee7cb1f319fc00bcf5d9
Ubuntu <azureuser@fiodar-a100-vmss-1-vmss000001.fall3ublhamenklcu35hj2caof.jx.internal.cloudapp.net>
2025-04-26 23:24:29 +0000

    some stats

--- job-scripts/llama-3-8b.sh
+++ job-scripts/llama-3-8b.sh
@@ -1,8 +1,8 @@
-TENSOR_PARALLEL_SIZES=(1 2 4 8)
+TENSOR_PARALLEL_SIZES=(2)
 for TENSOR_PARALLEL_SIZE in ${TENSOR_PARALLEL_SIZES[@]}; do
     export TENSOR_PARALLEL_SIZE=${TENSOR_PARALLEL_SIZE}
     cd ~/powertrace-sim/server
-    setsid bash serve-llama-3-8b.sh >/dev/null 2>&1 &
+    setsid bash serve-llama-3-8b.sh 2>&1 &
     SERVING_PID=$!                        
     SERVING_PGID=$(ps -o pgid= -p "$SERVING_PID" | tr -d ' ')
     while ! curl -s -f http://localhost:8000/health &> /dev/null; do
@@ -10,11 +10,7 @@ for TENSOR_PARALLEL_SIZE in ${TENSOR_PARALLEL_SIZES[@]}; do
         sleep 10
     done
     cd ~/powertrace-sim/client
-    POISSON_ARRIVAL_RATES=(0.125 0.25 0.5 1.0 2.0 4.0)
-    NUM_ITERATIONS=5
-    for ((i=0; i<NUM_ITERATIONS; i++)); do
-        POISSON_ARRIVAL_RATES+=(${POISSON_ARRIVAL_RATES[@]})
-    done
+    POISSON_ARRIVAL_RATES=(0.25 0.5 1.0 2.0 4.0 0.125 0.25 0.5 1.0 2.0 4.0 0.125 0.25 0.5 1.0 2.0 4.0 0.125 0.25 0.5 1.0 2.0 4.0)
     for POISSON_ARRIVAL_RATE in ${POISSON_ARRIVAL_RATES[@]}; do
         DATE_TIME=$(date '+%Y-%m-%d-%H-%M-%S')
         touch llama-3-8b_tp${TENSOR_PARALLEL_SIZE}_p${POISSON_ARRIVAL_RATE}_d${DATE_TIME}.csv
