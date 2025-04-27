fdedcc1afd22254d1492bf13aa8f8c46c366d17d
Grant <grant.wilkins371@gmail.com>
2025-04-27 12:28:52 -0700

    70b

--- job-scripts/llama-3-70b.sh
+++ job-scripts/llama-3-70b.sh
@@ -1,4 +1,4 @@
-TENSOR_PARALLEL_SIZES=(8)
+TENSOR_PARALLEL_SIZES=(4 8)
 for TENSOR_PARALLEL_SIZE in ${TENSOR_PARALLEL_SIZES[@]}; do
     export TENSOR_PARALLEL_SIZE=${TENSOR_PARALLEL_SIZE}
     cd ~/powertrace-sim/server
@@ -10,17 +10,13 @@ for TENSOR_PARALLEL_SIZE in ${TENSOR_PARALLEL_SIZES[@]}; do
         sleep 10
     done
     cd ~/powertrace-sim/client
-    POISSON_ARRIVAL_RATES=(0.125 0.25 0.5 1.0 2.0 4.0)
-    NUM_ITERATIONS=3
-    for ((i=0; i<NUM_ITERATIONS; i++)); do
-        POISSON_ARRIVAL_RATES+=(${POISSON_ARRIVAL_RATES[@]})
-    done
+    POISSON_ARRIVAL_RATES=(0.125 0.25 0.5 1.0 2.0 4.0 0.125 0.25 0.5 1.0 2.0 4.0 0.125 0.25 0.5 1.0 2.0 4.0 0.125 0.25 0.5 1.0 2.0 4.0 0.125 0.25 0.5 1.0 2.0 4.0)
     for POISSON_ARRIVAL_RATE in ${POISSON_ARRIVAL_RATES[@]}; do
         DATE_TIME=$(date '+%Y-%m-%d-%H-%M-%S')
         touch llama-3-70b_tp${TENSOR_PARALLEL_SIZE}_p${POISSON_ARRIVAL_RATE}_d${DATE_TIME}.csv
         nvidia-smi --query-gpu=timestamp,power.draw,utilization.gpu,memory.used --format=csv -lms 250 >> llama-3-70b_tp${TENSOR_PARALLEL_SIZE}_p${POISSON_ARRIVAL_RATE}_d${DATE_TIME}.csv &
         NVIDIA_SMI_PID=$!
-        python3 client.py --model-name meta-llama/Llama-3.1-70B-Instruct --api-key ${OPENAI_API_KEY} --tensor-parallel-size ${TENSOR_PARALLEL_SIZE} --poisson-arrival-rate ${POISSON_ARRIVAL_RATE} --date ${DATE_TIME}
+        python3 client_async.py --model-name meta-llama/Llama-3.1-70B-Instruct --api-key ${OPENAI_API_KEY} --tensor-parallel-size ${TENSOR_PARALLEL_SIZE} --poisson-arrival-rate ${POISSON_ARRIVAL_RATE} --date ${DATE_TIME}
         kill -9 ${NVIDIA_SMI_PID}
     done
     kill -TERM -- "-$SERVING_PGID"
