46703fbbe03e323eca4e1d1843c8c75dff3d8143
Grant <grant.wilkins371@gmail.com>
2025-05-18 02:02:52 -0700

    script

--- job-scripts/deepseek-r1-distill-70b.sh
+++ job-scripts/deepseek-r1-distill-70b.sh
@@ -2,7 +2,7 @@ TENSOR_PARALLEL_SIZES=(4 8)
 for TENSOR_PARALLEL_SIZE in ${TENSOR_PARALLEL_SIZES[@]}; do
     export TENSOR_PARALLEL_SIZE=${TENSOR_PARALLEL_SIZE}
     cd ~/powertrace-sim/server
-    setsid bash serve-deepseek-r1-distill-70b.sh >/dev/null 2>&1 &
+    setsid bash serve-deepseek-r1-distill-70b.sh 2>&1 &
     SERVING_PID=$!                        
     SERVING_PGID=$(ps -o pgid= -p "$SERVING_PID" | tr -d ' ')
     while ! curl -s -f http://localhost:8000/health &> /dev/null; do
@@ -10,13 +10,14 @@ for TENSOR_PARALLEL_SIZE in ${TENSOR_PARALLEL_SIZES[@]}; do
         sleep 10
     done
     cd ~/powertrace-sim/client
-    POISSON_ARRIVAL_RATES=(0.125 0.25 0.5 1.0 2.0 4.0)
+    POISSON_ARRIVAL_RATES=(0.125 0.25 0.5 1.0 2.0 4.0 0.125 0.25 0.5 1.0 2.0 4.0 0.125 0.25 0.5 1.0 2.0 4.0)
     for POISSON_ARRIVAL_RATE in ${POISSON_ARRIVAL_RATES[@]}; do
         DATE_TIME=$(date '+%Y-%m-%d-%H-%M-%S')
         touch deepseek-r1-distill-70b_tp${TENSOR_PARALLEL_SIZE}_p${POISSON_ARRIVAL_RATE}_d${DATE_TIME}.csv
         nvidia-smi --query-gpu=timestamp,power.draw,utilization.gpu,memory.used --format=csv -lms 250 >> deepseek-r1-distill-70b_tp${TENSOR_PARALLEL_SIZE}_p${POISSON_ARRIVAL_RATE}_d${DATE_TIME}.csv &
         NVIDIA_SMI_PID=$!
-        python3 client.py --model-name deepseek-ai/DeepSeek-R1-Distill-Llama-70B --api-key ${OPENAI_API_KEY} --tensor-parallel-size ${TENSOR_PARALLEL_SIZE} --poisson-arrival-rate ${POISSON_ARRIVAL_RATE} --date ${DATE_TIME}
+        NUM_PROMPTS=$(printf "%.0f" $(echo "600 * ${POISSON_ARRIVAL_RATE}" | bc))
+        python3 benchmark_serving.py --model deepseek-ai/DeepSeek-R1-Distill-Llama-70B --backend vllm --dataset-name sharegpt --dataset-path ${HOME}/ShareGPT_V3_unfiltered_cleaned_split.json --tensor-parallel-size ${TENSOR_PARALLEL_SIZE} --request-rate ${POISSON_ARRIVAL_RATE} --num-prompts ${NUM_PROMPTS} --endpoint /v1/completions --save-result --save-detailed
         kill -9 ${NVIDIA_SMI_PID}
     done
     kill -TERM -- "-$SERVING_PGID"
