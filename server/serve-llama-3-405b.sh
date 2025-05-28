24a4686d98f7698f67679f8afa32e3ee7a86aa16
Ubuntu <azureuser@gpu-vmss000002.xygmcdbjwn0ezjkgaeoryjq4rf.cx.internal.cloudapp.net>
2025-05-28 02:07:23 +0000

    70b stats

--- server/serve-llama-3-405b.sh
+++ server/serve-llama-3-405b.sh
@@ -1,2 +1,2 @@
 cd ~/powertrace-sim/server/
-vllm serve meta-llama/Llama-3.1-405B-Instruct  --tensor-parallel-size ${TENSOR_PARALLEL_SIZE} --api-key ${OPENAI_API_KEY}
+vllm serve meta-llama/Llama-3.1-405B-Instruct-FP8  --tensor-parallel-size ${TENSOR_PARALLEL_SIZE} --max-num-seqs 64 
